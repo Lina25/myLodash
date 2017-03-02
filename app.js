612bc1daa952ade3c398ab366e2ed4a446d793dc
Lina25 <lina.kurylo@gmail.com>
2017-03-03 00:10:13 +0300

    add functions

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-(function(){
-	// max 
-	console.log("Test max");
-	var array = [-1, 0, 6, -9];
-	console.log(array);
-	var maxResult = myLodash.max(array);
-	console.log(maxResult);
-})();
+//(function(){
+//	// max 
+//	console.log("Test max");
+//	var array = [-1, 0, 6, -9];
+//	console.log(array);
+//	var maxResult = myLodash.max(array);
+//	console.log(maxResult);
+//})();
+
+
+// (function(){
+//	// min 
+//	console.log("Test min");
+//	var array = [-1, 2, 6, -9];
+//	console.log(array);
+//	var minResult = myLodash.min(array);
+//	console.log(minResult);
+//})();
+
+
+
+//(function(){
+//	// sum 
+//	console.log("Test sum");
+//	var array = [2, 9];
+//	console.log(array);
+//	var sumResult = myLodash.sum(array);
+//	console.log(sumResult);
+//})();
+
+
+//(function(){
+//	// random 
+//	console.log("Test random");
+//	var min = 2;
+//	var max = 10;
+//	console.log(min, max);
+//	var randomResult = myLodash.random(min, max);
+//	console.log(randomResult);
+//})();
+
+
--- myLodash.js
+++ myLodash.js
@@ -6,55 +6,54 @@
     
     
     //max
-    myLodash.max = function (array) {
-      var max = array[0];
-      for (var i = 0; i < array.length; i++) {
-        if (max < array[i]) {
-          max = array[i];
-        }
-      }
-      return max;
-    };
+//    myLodash.max = function (array) {
+//      var max = array[0];
+//      for (var i = 0; i < array.length; i++) {
+//        if (max < array[i]) {
+//          max = array[i];
+//        }
+//      }
+//      return max;
+//    };
     
     
     //min
-    myLodash.min = function (array) {
-      var min = array[0];
-      for (var i = 0; i < array.length; i++) {
-        if (min > array[i]) {
-          min = array[i];
-        }
-      }
-      return min;
-    };
+//    myLodash.min = function (array) {
+//      var min = array[0];
+//      for (var i = 0; i < array.length; i++) {
+//        if (min > array[i]) {
+//          min = array[i];
+//        }
+//      }
+//      return min;
+//    };
+//    
+//    
+//    //sum
+//    myLodash.sum = function (array) {
+//      var sum = 0;
+//      for (var i = 0; i < array.length; i++) {
+//        sum += array[i];
+//      }
+//      return sum;
+//    };
+//    
+//
+//    //random
+//    myLodash.random = function (min, max) {
+//      return Math.floor(Math.random() * (max - min + 1)) + min;
+//    }
     
-    
-    //sum
-    myLodash.sum = function (array) {
-      var numbers = [];
-      var sum = 0;
-      for (var i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
-      }
-      return sum;
-    };
-    
-
-    //random
-    myLodash.random = function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    return random;
-    
-    
-    //range
+//    
+//    
+//    //range
     myLodash.range = function (start, end, step) {
       var arr = [];
       for (var i = start; i <= end; i += step) {
         arr.push(i);
       }
       return range;
-    }
+    };
 
     return myLodash;
   }
