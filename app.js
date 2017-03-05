108ca6c2491969bcb7acec46aa352f80983ec80b
Lina25 <lina.kurylo@gmail.com>
2017-03-06 01:00:17 +0300

    add functions

--- app.js
+++ app.js
@@ -1,42 +1,177 @@
-//(function(){
-//	// max 
-//	console.log("Test max");
-//	var array = [-1, 0, 6, -9];
-//	console.log(array);
-//	var maxResult = myLodash.max(array);
-//	console.log(maxResult);
-//})();
+(function(){
+	// max 
+	console.log("Test max");
+	var array = [-1, 0, 6, -9];
+	console.log(array);
+	var maxResult = myLodash.max(array);
+	console.log(maxResult);
+})();
 
 
-// (function(){
-//	// min 
-//	console.log("Test min");
-//	var array = [-1, 2, 6, -9];
-//	console.log(array);
-//	var minResult = myLodash.min(array);
-//	console.log(minResult);
-//})();
+ (function(){
+	// min 
+	console.log("Test min");
+	var array = [-1, 2, 6, -9];
+	console.log(array);
+	var minResult = myLodash.min(array);
+	console.log(minResult);
+})();
 
 
 
-//(function(){
-//	// sum 
-//	console.log("Test sum");
-//	var array = [2, 9];
-//	console.log(array);
-//	var sumResult = myLodash.sum(array);
-//	console.log(sumResult);
-//})();
+(function(){
+	// sum 
+	console.log("Test sum");
+	var array = [2, 9];
+	console.log(array);
+	var sumResult = myLodash.sum(array);
+	console.log(sumResult);
+})();
 
 
-//(function(){
-//	// random 
-//	console.log("Test random");
-//	var min = 2;
-//	var max = 10;
-//	console.log(min, max);
-//	var randomResult = myLodash.random(min, max);
-//	console.log(randomResult);
-//})();
+(function(){
+	// random 
+	console.log("Test random");
+	var min = 2;
+	var max = 10;
+	console.log(min, max);
+	var randomResult = myLodash.random(min, max);
+	console.log(randomResult);
+})();
+
+
+(function(){
+	// range
+	console.log("Test range");
+	var start = 2;
+	var end = 20;
+	var step = 2;
+  
+	console.log(start, end, step);
+	var rangeResult = myLodash.range(start, end, step);
+	console.log(rangeResult);
+})();
+
+
+
+(function(){
+	// isArray
+	console.log("Test isArray");
+	var array = 5;
+	console.log(array);
+	var arrayResult = myLodash.isArray(array);
+	console.log(arrayResult);
+})();
+   
+
+(function(){
+	// isBoolean
+	console.log("Test isBoolean");
+	var value = true;
+	console.log(value);
+	var booleanResult = myLodash.isBoolean(value);
+	console.log(booleanResult);
+})();
+
+
+(function(){
+	// isEmpty
+	console.log("Test isEmpty");
+	var value = {};
+	console.log(value);
+	var emptyResult = myLodash.isEmpty(value);
+	console.log(emptyResult);
+})();
+
+
+(function(){
+	// clamp
+	console.log("Test clamp");
+	var number = 1;
+    var lower = 2;
+    var upper = 10;
+	console.log(number, lower, upper);
+	var clampResult = myLodash.clamp(number, lower, upper);
+	console.log(clampResult);
+})();
+
+
+(function(){
+	// remove
+	console.log("Test remove");
+    var arr = [2,1,3,5,9];
+	var number = 9;
+	console.log(arr, number);
+	var removeResult = myLodash.removeByValue(arr, number);
+	console.log(removeResult);
+})();
+
+
+(function(){
+	// difference
+	console.log("Test difference");
+    var arr1 = [2,1,3,5];
+    var arr2 = [7,8];
+	var diffResult = myLodash.difference(arr1, arr2);
+	console.log(diffResult);
+})();
+
+
+
+(function(){
+	// drop
+	   console.log("Test drop");
+        var arr = [2,3,4];
+        console.log(arr);
+        var start = 0; 
+        var deleteCount = 2;
+	var dropResult = myLodash.drop(arr, start, deleteCount);
+	console.log(dropResult);
+})();
+
+
+(function(){ //?????????
+	// findIndex
+	   console.log("Test findIndex");
+        var array = [2,6,8];
+        console.log(array);
+        var number = 2;
+	var findIndexResult = myLodash.getIndex(??);
+	console.log(findIndexResult);
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- myLodash.js
+++ myLodash.js
@@ -6,58 +6,134 @@
     
     
     //max
-//    myLodash.max = function (array) {
-//      var max = array[0];
-//      for (var i = 0; i < array.length; i++) {
-//        if (max < array[i]) {
-//          max = array[i];
-//        }
-//      }
-//      return max;
-//    };
+    myLodash.max = function (array) {
+      var max = array[0];
+      for (var i = 0; i < array.length; i++) {
+        if (max < array[i]) {
+          max = array[i];
+        }
+      }
+      return max;
+    };
     
     
     //min
-//    myLodash.min = function (array) {
-//      var min = array[0];
-//      for (var i = 0; i < array.length; i++) {
-//        if (min > array[i]) {
-//          min = array[i];
-//        }
-//      }
-//      return min;
-//    };
-//    
-//    
-//    //sum
-//    myLodash.sum = function (array) {
-//      var sum = 0;
-//      for (var i = 0; i < array.length; i++) {
-//        sum += array[i];
-//      }
-//      return sum;
-//    };
-//    
-//
-//    //random
-//    myLodash.random = function (min, max) {
-//      return Math.floor(Math.random() * (max - min + 1)) + min;
-//    }
+    myLodash.min = function (array) {
+      var min = array[0];
+      for (var i = 0; i < array.length; i++) {
+        if (min > array[i]) {
+          min = array[i];
+        }
+      }
+      return min;
+    };
     
-//    
-//    
-//    //range
+    
+    //sum
+    myLodash.sum = function (array) {
+      var sum = 0;
+      for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+      }
+      return sum;
+    };
+    
+    
+    //random
+    myLodash.random = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      };
+    
+    
+      //range
     myLodash.range = function (start, end, step) {
       var arr = [];
       for (var i = start; i <= end; i += step) {
         arr.push(i);
       }
-      return range;
+      return arr;
     };
-
+    
+    
+    //isArray
+    myLodash.isArray = function (array) {
+      if (array instanceof Array) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    };
+    
+    
+    //isBoolean
+    myLodash.isBoolean = function (value) {
+      return typeof value === "boolean";
+    };
+    
+    
+    //isEmpty
+    myLodash.isEmpty = function (value) {
+      if (value === undefined) return true;
+      if (typeof (value) == 'function' || typeof (value) == 'number' || typeof (value) == 'boolean') return false;
+      if (value == null || value.length === 0) return true;
+      if (typeof (value) == "object") {
+        var sample = true;
+        for (var i in value) sample = false;
+        return sample;
+      }
+      return false;
+    };
+    
+    
+    //clamp
+    myLodash.clamp = function (number, lower, upper) {
+        var clamped = Math.max(lower, Math.min(number, upper));
+        return clamped;
+        //return number <= lower ? lower : number >= upper ? upper : number;
+      };
+    
+    
+    //remove
+    myLodash.removeByValue = function (arr, number) {
+      var index = arr.indexOf(number);
+      if (index > -1) {
+        arr.splice(index, 1);
+      }
+      return arr;
+    };
+    
+    
+    //difference
+    myLodash.difference = function (arr1, arr2) {
+      var newArr = [];
+      arr1.forEach(function (val) {
+        if (arr2.indexOf(val) < 0) newArr.push(val);
+      });
+      arr2.forEach(function (val) {
+        if (arr1.indexOf(val) < 0) newArr.push(val);
+      });
+      return newArr;
+    };
+    
+    
+    //drop
+    myLodash.drop = function (arr, start, deleteCount) {
+      var arr;
+      arr.splice(start, deleteCount);
+      return arr;
+    };
+    
+    
+    //findIndex
+    myLodash.getIndex = function () {
+      var array = [];
+      var number;
+      var g = array.indexOf(number);
+      return g;
+    };
     return myLodash;
   }
-
   if (typeof (myLodash) == 'undefined') {
     window.myLodash = defineLodash();
   }
